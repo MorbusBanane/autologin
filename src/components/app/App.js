e7471a7b32d1d2f957b48a007954a667ce4084ba
Morbus Banane <morbusbanane@gmail.com>
2018-09-04 15:14:33 +0200

    Jetzt mit Deutscher Fehlermeldung

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -33,7 +33,25 @@ class App extends Component {
         console.log(error);
       });
 
-    return <div className="App">Hallo!</div>;
+    return (
+    <div className="App">
+      Fehler!!!
+      <br />
+      <br />
+      Failed to load
+      https://www.diesiedleronline.de/:
+      Redirect from 'https://www.diesiedleronline.de/' to 'https://www.diesiedleronline.de/de'
+      has been blocked by CORS policy:
+      The 'Access-Control-Allow-Origin' header has a value 'https://www.diesiedleronline.de'
+      that is not equal to the supplied origin. Origin 'http://localhost:3000' is therefore not allowed access.
+      App.js:33 Error: Network Error
+        at createError (createError.js:16)
+        at XMLHttpRequest.handleError (xhr.js:87)
+      <br />
+      <br />
+      See console.log...
+    </div>
+  )
   }
 }
 
